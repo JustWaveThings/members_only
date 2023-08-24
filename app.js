66410cba08d80bd81a5f07abf613e9299236159f
JustWaveThings <justwavethings@proton.me>
2023-08-24 19:32:41 -0400

    Add auth related packages

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-require('dotenv').config();
-const debug = require('debug')('app');
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
+require("dotenv").config();
+const debug = require("debug")("app");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const Schema = mongoose.Schema;
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+mongoose.set("strictQuery", false);
 
-const compression = require('compression');
-const helmet = require('helmet');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+const compression = require("compression");
+const helmet = require("helmet");
 
 const app = express();
 
-const RateLimit = require('express-rate-limit');
+const RateLimit = require("express-rate-limit");
 const limiter = RateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 20,
@@ -28,33 +34,33 @@ app.use(limiter); // apply to all requests
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
-      'script-src': 'self',
+      "script-src": "self",
     },
   })
 );
 
 const mongoDB = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.dmc0his.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-main().catch(err => debug(err));
+main().catch((err) => debug(err));
 
 async function main() {
   await mongoose.connect(mongoDB);
 }
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(compression());
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -65,11 +71,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
