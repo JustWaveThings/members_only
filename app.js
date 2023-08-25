e0c4e54b493cc72c02717f7ad5a749a6734a5f15
JustWaveThings <justwavethings@proton.me>
2023-08-24 22:40:55 -0400

    Refactor to remove schema from app.js

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const bcrypt = require("bcryptjs");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const Schema = mongoose.Schema;
 
 mongoose.set("strictQuery", false);
 
