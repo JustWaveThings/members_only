4eea776829ad6802c2f80f36422d2c817c997e42
JustWaveThings <justwavethings@proton.me>
2023-09-09 17:00:58 -0400

    Update conditional logic for isMember

--- utils/authMiddleware.js
+++ utils/authMiddleware.js
@@ -5,28 +5,40 @@ module.exports.isAuth = (req, res, next) => {
     res.render("error", {
       message:
         "You are not authorized to view this resource because you are not logged in.",
+      error: {
+        status: null,
+        stack: null,
+      },
     });
   }
 };
 
 module.exports.isMember = (req, res, next) => {
-  if (req.isAuthenticated() && req.user.member) {
+  if (req.isAuthenticated() && res.locals.member) {
     next();
   } else {
     res.render("error", {
       message:
         "You are not authorized to view this resource because you are not a member.",
+      error: {
+        status: null,
+        stack: null,
+      },
     });
   }
 };
 
 module.exports.isAdmin = (req, res, next) => {
-  if (req.isAuthenticated() && req.user.admin) {
+  if (req.isAuthenticated() && res.locals.admin) {
     next();
   } else {
     res.render("error", {
       message:
         "You are not authorized to view this resource because you are not an Admin.",
+      error: {
+        status: null,
+        stack: null,
+      },
     });
   }
 };
