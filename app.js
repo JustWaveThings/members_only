cd9e72c8ed40360bbd0f0e4ae57888811f1d120e
JustWaveThings <justwavethings@proton.me>
2023-09-10 20:09:19 -0400

    Remove unneeded locals

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-require("dotenv").config();
-const debug = require("debug")("app");
-const createError = require("http-errors");
-const express = require("express");
-const passport = require("passport");
-const expressLayouts = require("express-ejs-layouts");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const session = require("express-session");
+require('dotenv').config();
+const debug = require('debug')('app');
+const createError = require('http-errors');
+const express = require('express');
+const passport = require('passport');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
 
-const user_controller = require("./models/users");
-const message_controller = require("./models/messages");
+const user_controller = require('./models/users');
+const message_controller = require('./models/messages');
 
-const database = require("./utils/database");
+const database = require('./utils/database');
 
-const indexRouter = require("./routes/index");
+const indexRouter = require('./routes/index');
 
-const compression = require("compression");
-const helmet = require("helmet");
-const limiter = require("./utils/rateLimit");
+const compression = require('compression');
+const helmet = require('helmet');
+const limiter = require('./utils/rateLimit');
 
 const app = express();
 
@@ -32,14 +32,14 @@ app.use(database);
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
-      "script-src": "self",
+      'script-src': 'self',
     },
   })
 );
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // express session setup
 
@@ -56,7 +56,7 @@ app.use(
 
 // passport setup
 
-require("./utils/passport");
+require('./utils/passport');
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -66,15 +66,12 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.locals.session = req.session;
-  res.locals.title = "Members Only";
+  res.locals.title = 'Members Only';
   next();
 });
 
 app.use((req, res, next) => {
   if (req.isAuthenticated()) {
-    res.locals.username = req.user.username;
-    res.locals.password = req.user.password;
-    res.locals.firstname = req.user.firstName;
     res.locals.member = req.user.member;
     res.locals.admin = req.user.admin;
     res.locals.signedin = true;
@@ -94,14 +91,14 @@ app.use(expressLayouts);
 // compression
 app.use(compression());
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
-app.use("/", indexRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -112,11 +109,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
-app.listen(3001, () => console.log("App listening on port 3001!"));
+app.listen(3001, () => console.log('App listening on port 3001!'));
